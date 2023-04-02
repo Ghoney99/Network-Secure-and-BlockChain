f618f420efa41b0e81d4c0448358e84cb9cfcf54
ghoney <wifi9195@naver.com>
2023-04-02 23:42:14 +0900

    수정

--- 5.2_Bitcoin Mining.py
+++ 5.2_Bitcoin Mining.py
@@ -1,27 +1,37 @@
-import hashlib
 import time
+import hashlib
 
-message = input("메시지의 내용? ")
+# 메시지와 target bits 입력
+msg = input("메시지의 내용? ")
 target_bits = input("Target bits? ")
 
-max_nonce = 2 ** 32 - 1
-target = int(target_bits, 16)
-print("Target: 0x{:064x}".format(target))
+# target 값 계산
+exponent = int(target_bits[:2], 16)
+coefficient = int(target_bits[2:], 16)
+target = coefficient * 2 ** (8 * (exponent - 3))
 
+# 메시지와 extra nonce 초기화
+message = msg.encode()
 extra_nonce = int(time.time())
-print("메시지: {}, Extra nonce: {}".format(message, extra_nonce))
 
 start_time = time.time()
 
-for nonce in range(max_nonce + 1):
-    data = message.encode() + extra_nonce.to_bytes(4, byteorder='big') + nonce.to_bytes(4, byteorder='big')
-    hash_result = hashlib.sha256(hashlib.sha256(data).digest()).digest()
-    hash_int = int.from_bytes(hash_result, byteorder='big')
-
-    if hash_int < target:
-        print("nonce: {}".format(nonce))
-        print("실행 시간: {}초".format(time.time() - start_time))
-        print("Hash result: 0x{:064x}".format(hash_int))
+# nonce 값 계산
+nonce = 0
+while True:
+    # 해시 계산
+    hash_result = hashlib.sha256(message + extra_nonce.to_bytes(4, 'little') + nonce.to_bytes(4, 'little')).digest()
+    
+     # 결과 출력
+    if int.from_bytes(hash_result, 'big') < target:
+        print(f"Target: 0x{target:064x}")
+        print(f"메시지: {msg}, Extra nonce: {extra_nonce}, nonce: {nonce}")
+        print(f"실행 시간: {time.time() - start_time:.9f}초")
+        print(f"Hash result: 0x{hash_result.hex()}")
         break
-else:
-    print("POW를 만족하는 nonce가 없습니다.")
+    
+    # nonce 값 변경
+    nonce += 1
+    if nonce == 2**32:
+        extra_nonce = int(time.time())
+        nonce = 0
